2904ee0ddf658320f2dad199d3ea17e766e524ba
jjborrelli <borrejj@gmail.com>
2014-06-14 14:27:08 -0400

    add code for python module

---
+++ codeACADEMY.py
@@ -0,0 +1,60 @@
+# CodeAcademy Python course
+
+# PYTHON SYNTAX
+print "Welcome to Python"
+
+# Set a variable
+my_variable = 10
+
+#Booleans
+my_int = 7
+my_float = 1.23
+my_bool = True
+
+# Reassign a variable
+my_int = 3
+
+# Whitespace
+# Whitespace is used to structure code
+# Properly indent code:
+def spam():
+    eggs = 12
+    return eggs
+    
+print spam()
+
+
+# interpretation
+spam = True
+eggs = False
+
+"""
+Triple quotation marks allow you to write 
+multi line comments
+"""
+
+# Math
+count_to = 10 + 2 
+print count_to
+
+## Exponents
+eggs = 10**2
+print eggs
+
+# The modulo returns the remainder from division
+# use %
+spam = 5 % 2
+print spam
+
+# Tip Calculator
+meal = 44.50
+tax = 6.75 / 100
+tip = 15.0 / 100
+
+meal = meal + meal * tax
+total = meal + meal * tip
+
+print("%.2f" % total)
+
+# Strings and Console Output
+
